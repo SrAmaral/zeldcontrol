a48303a6aa3f338eb719659be9150dbc3819df50
Eduardo Amaral <edu.amaral8@gmail.com>
2024-06-13 12:39:40 -0300

    Add envoriment variables in docker compose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <main>
-      <h1>zeld Control</h1>
+      <h1>Zeld Control</h1>
       <Link href="/login">Login</Link>
     </main>
   );
