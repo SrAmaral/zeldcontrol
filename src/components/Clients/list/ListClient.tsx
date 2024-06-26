774d0e6b17880ef3e22d2a14468e9102d5acc261
Eduardo Amaral <edu.amaral8@gmail.com>
2024-06-26 13:17:06 -0300

    add clear filter

--- src/components/Clients/list/ListClient.tsx
+++ src/components/Clients/list/ListClient.tsx
@@ -39,7 +39,7 @@ export default function ListClientComponent({
   }, [filter, clients]);
 
   const clearFilter = () => {
-    // initFilters();
+    setFilter("");
   };
 
   const renderHeader = () => {
