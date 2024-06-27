a12a0b876c8f9c450e8135b6bf150383dd5048a3
Eduardo Amaral <edu.amaral8@gmail.com>
2024-06-27 14:56:55 -0300

    Alter Theme Folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,9 @@ import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
 import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/primereact.css";
-import "~/styles/theme/theme-light/indigo/theme.css";
-// import "../styles/Demo.css";
-import "../styles/Layout.css";
+import "../../public/theme/theme-light/indigo/theme.css";
+import "../styles/demo/Demos.scss";
+import "../styles/layout/layout.scss";
 
 interface RootLayoutProps {
   children: React.ReactNode;
