9bae98de8591f15d25cb567acb8ced2f98177fc5
Eduardo Amaral <edu.amaral8@gmail.com>
2024-06-27 15:43:24 -0300

    Alter Theme Folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { LayoutProvider } from "../layout/context/layoutcontext";
 
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
 import { PrimeReactProvider } from "primereact/api";
-import "primereact/resources/primereact.css";
-import "../../public/theme/theme-light/indigo/theme.css";
-import "../styles/demo/Demos.scss";
-import "../styles/layout/layout.scss";
+// import "../styles/Demo.css";
+import "../styles/Layout.css";
+import "../styles/primereact.css";
+import "../styles/theme.css";
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -16,9 +17,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <PrimeReactProvider>
-          <LayoutProvider>{children}</LayoutProvider>
-        </PrimeReactProvider>
+        <AppRouterCacheProvider>
+          <PrimeReactProvider>
+            <LayoutProvider>{children}</LayoutProvider>
+          </PrimeReactProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,10 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
+  corePlugins: {
+    preflight: false,
+  },
+  important: '#__next',
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
